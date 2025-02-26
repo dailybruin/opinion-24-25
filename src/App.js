3f88bf03f0d90fcdedf399822802019641404c2d
kcabalonga <cabalongakirsten@gmail.com>
2025-02-25 20:03:28 -0800

    completed slide11 mobile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Slide10 from "./components/Slide10_Mobile";
+import Slide11 from "./components/Slide11_Mobile";
 
 function App() {
   const [ data, setData ] = useState(null);
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Header/>
       Hello Daily Bruin!
-      <Slide10 article={data.articles[9]}/>
+      <Slide11 article={data.articles[10]}/>
       <Footer/>
     </div>
   );
