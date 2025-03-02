b6d376dbd4c9d38b028e3edfb6563434915c6b70
nothenrynguyen <nothenrynguyen@gmail.com>
2025-03-02 00:19:54 -0800

    Add Slide 5 & Slide 6 (desktop + mobile) components and assets

--- src/components/Slide5.js
+++ src/components/Slide5.js
@@ -110,74 +110,63 @@ const Slide5 = ({props}) => (
 );
 
 export default Slide5;
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import PhoneImage from "../images/Group55.png";
 
-const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-color: #FBE5B6;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2rem;
-    box-sizing: border-box;
+const Background = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: #FBE5B6;
+  overflow: hidden;
 `;
 
-const LeftColumn = styled.div`
-    width: 45%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    font-family: 'Baskerville', serif;
+const PhoneContainer = styled.div`
+  width: 446px;
+  height: 819px;
+  background-image: url(${PhoneImage});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  margin-right: calc(25vw - (446px / 2));
 `;
 
-const Title = styled.h1`
-    font-family: 'Passion One', sans-serif;
-    font-size: 3.5rem;
-    color: #547966;
-    text-transform: uppercase;
-    margin-bottom: 1rem;
+const ScrollableText = styled.div`
+  width: 80%;
+  height: 72%;
+  margin-top: 90px;
+  overflow-y: auto;
+  padding: 1rem;
+  font-family: "Baskervville", serif;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #383765;
+  background-color: transparent;
 `;
 
-const BodyText = styled.p`
-    font-size: 1.2rem;
-    line-height: 1.6;
-    color: black;
-`;
+const Slide5 = () => (
+  <Background>
+    <PhoneContainer>
+      <ScrollableText>
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Per tortor duis sociosqu interdum pellentesque turpis. Ligula fames per id porta sed felis. Fermentum urna accumsan ligula convallis phasellus quis. Proin ipsum efficitur rutrum bibendum aptent non. Faucibus ac netus justo rutrum posuere sagittis libero ut.  
+        <br/><br/>
+        Hendrerit etiam nisi suspendisse justo amet. In felis nibh lacus efficitur platea leo nisl ac cubilia. Torquent efficitur natoque natoque quis aenean placerat; posuere justo congue. Maecenas molestie luctus conubia; orci condimentum dapibus est. At arcu sed id lacinia ornare lorem mauris. Eros sociosqu cursus suscipit metus sodales sem phasellus neque fermentum. Volutpat sagittis ex finibus potenti dis. Accumsan amet cubilia litora nascetur pellentesque malesuada.
+        <br/><br/>
+        Leo congue hendrerit egestas nisi ex ipsum per tempor. Curae consequat diam accumsan aliquam suscipit lacus consectetur. Feugiat duis tempus pharetra suspendisse hendrerit senectus. Curabitur natoque tincidunt cubilia placerat est felis. Ligula nibh convallis elementum morbi est auctor orci scelerisque etiam. Est velit netus adipiscing aliquam nullam diam nisi.
+        <br/><br/>
+        Feugiat mus parturient venenatis amet bibendum porttitor cursus aliquam. Quisque hac justo curae lectus conubia ante cubilia natoque. Ac primis conubia magnis accumsan sociosqu in fames. Orci accumsan eget montes sociosqu facilisis a. Diam dis leo suspendisse congue himenaeos inceptos curabitur. Magna vulputate justo ornare, blandit mus id ante.
+        <br/><br/>
+        Litora ac sociosqu eleifend molestie nostra, nam porttitor viverra. Ac felis hac urna hac tincidunt. Semper mus curae luctus eu accumsan tempus. Rutrum litora metus vivamus feugiat conubia vestibulum sit vehicula himenaeos. Sapien consectetur elementum dolor ut; justo natoque habitant senectus? Gravida fusce accumsan porttitor fusce sociosqu nostra. Suscipit turpis magnis aliquam cras mattis laoreet maecenas erat eu.
+      </ScrollableText>
+    </PhoneContainer>
+  </Background>
+);
 
-const RightColumn = styled.div`
-    width: 45%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const PlaceholderImage = styled.div`
-    width: 90%;
-    height: 80%;
-    background-color: #DDD;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 1.5rem;
-    color: #547966;
-    border: 4px solid #547966;
-`;
-
-export default function Slide5() {
-    return (
-        <Container>
-            <LeftColumn>
-                <Title>E-IDENTITY</Title>
-                <BodyText>
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Per tortor duis sociosqu interdum pellentesque turpis.
-                </BodyText>
-            </LeftColumn>
-            <RightColumn>
-                <PlaceholderImage>
-                    Image Placeholder
-                </PlaceholderImage>
-            </RightColumn>
-        </Container>
-    );
-}
+export default Slide5;
--- src/components/Slide6_Mobile.js
+++ src/components/Slide6_Mobile.js
@@ -92,59 +92,97 @@ const Slide6Mobile = () => {
 };
 
 export default Slide6Mobile;
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import MapImage from "../images/map.png";
 
-const Container = styled.div`
-    width: 100vw;
+const SlideWrapper = styled.div`
     height: 100vh;
-    background-color: #6A4C93;
+    width: 100vw;
+    overflow: hidden;
+    background-color: #393766;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-    font-family: 'Baskerville', serif;
-    color: white;
-    text-align: center;
     padding: 1rem;
     box-sizing: border-box;
 `;
 
-const Title = styled.h1`
-    font-family: 'Passion One', sans-serif;
-    font-size: 2.5rem;
+const Title = styled.div`
+    color: #FBE6B6;
+    font-family: "Passion One", sans-serif;
+    font-size: 2rem;
+    font-weight: bold;
     text-transform: uppercase;
-    margin-top: 1rem;
-    margin-bottom: 0.5rem;
+    text-align: center;
+    margin: 1rem 0;
 `;
 
-const BodyText = styled.p`
-    font-size: 1rem;
-    line-height: 1.5;
-    margin-bottom: 1rem;
+const Map = styled.img`
+    width: 90%;
+    max-width: 400px;
+    margin: 0 auto;
 `;
 
-const PlaceholderImage = styled.div`
-    width: 200px;
-    height: 200px;
-    background-color: #DDD;
+const TextContainer = styled.div`
+    color: #FBE6B6;
+    font-family: "Baskervville", serif;
+    font-size: 12px;
+    line-height: 1.4;
+    text-align: left;
+    width: 100%;
+    height: 30%;
+    overflow-y: auto;
+    padding: 0.5rem;
+    box-sizing: border-box;
+    background-color: transparent;
+
+    p {
+        margin-bottom: 0.75rem;
+    }
+`;
+
+const PhotoBoxContainer = styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
-    font-size: 0.8rem;
-    color: #6A4C93;
-    border: 3px solid white;
+    gap: 5px;
+    margin-top: 0.5rem;
 `;
 
-export default function Slide6_Mobile() {
+const PhotoBox = styled.div`
+    width: 90px;
+    height: 90px;
+    background-color: #FBE6B6;
+`;
+
+const Credit = styled.div`
+    font-family: "Baskervville", serif;
+    font-size: 12px;
+    color: #FBE6B6;
+    margin-top: 0.5rem;
+    text-align: center;
+`;
+
+const Slide6Mobile = () => {
     return (
-        <Container>
-            <Title>EXPEDITION</Title>
-            <BodyText>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Per tortor duis sociosqu interdum pellentesque turpis.
-            </BodyText>
-            <PlaceholderImage>
-                Image Placeholder
-            </PlaceholderImage>
-        </Container>
+        <SlideWrapper>
+            <Title>
+                TITLE OF THE STORY CAN GO HERE TITLE OF THE STORY CAN GO HERE
+            </Title>
+            <Map src={MapImage} alt="World Map" />
+            <TextContainer>
+                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Per tortor duis sociosqu interdum pellentesque turpis. Ligula fames per id porta sed felis. Fermentum urna accumsan ligula convallis phasellus quis. Proin ipsum efficitur rutrum bibendum aptent non. Faucibus ac netus justo rutrum posuere sagittis libero ut.</p>
+                <p>Hendrerit etiam nisi suspendisse justo amet. In felis nibh lacus efficitur platea leo nisl ac cubilia. Torquent efficitur natoque natoque quis aenean placerat; posuere justo congue. Maecenas molestie luctus conubia; orci condimentum dapibus est. At arcu sed id lacinia ornare lorem mauris. Eros sociosqu cursus suscipit metus sodales sem phasellus neque fermentum. Volutpat sagittis ex finibus potenti dis. Accumsan amet cubilia litora nascetur pellentesque malesuada.</p>
+            </TextContainer>
+            <PhotoBoxContainer>
+                <PhotoBox />
+                <PhotoBox />
+                <PhotoBox />
+            </PhotoBoxContainer>
+            <Credit>(Yeejee Kim/Daily Bruin staff)</Credit>
+        </SlideWrapper>
     );
-}
+};
+
+export default Slide6Mobile;
